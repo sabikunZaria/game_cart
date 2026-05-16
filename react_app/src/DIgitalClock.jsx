6fb379b0e178d0204d7981b3885df12328f8102b
TI ZARIR <sabikunzaria@gmail.com>
2026-05-16 15:44:01 +0600

    done

---
+++ react_app/src/DIgitalClock.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+function DigitalClock() {
+    const[time,setTime]=useState(new Date());
+
+    useEffect(()=>{
+        const timerId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+    return () => 
+        {clearInterval(timerId);}
+    },[]);
+
+    function formatTime() {
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        const meridiem = hours >= 12 ? 'PM' : 'AM';
+
+        hours = hours % 12 || 12;
+return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+    function padZero(number) {
+        return (number < 10 ? `0` : "") + number;
+    }
+
+    return(        
+    <div className="clock-container">
+        <div className="clock">
+            <span>{formatTime()}</span>
+        </div>
+        
+    </div>
+
+    );
+}
+
+export default DigitalClock;
